0fdf06a8f0e2957468c927744cac76654cee3828
TyroneYvesChen <243307272@qq.com>
2020-04-20 21:37:24 +0800

    ''

---
+++ EventEmitter.js
@@ -0,0 +1,24 @@
+class EventEmitter {
+  constructor() {
+    this._eventpool = {}
+  }
+  on(event, callback) {
+    this._eventpool[event]
+      ? this._eventpool[event].push(callback)
+      : (this._eventpool[event] = [callback])
+  }
+  emit(event, ...args) {
+    this._eventpool[event] && this._eventpool[event].forEach(cb => cb(...args))
+  }
+  off(event) {
+    if (this._eventpool[event]) {
+      delete this._eventpool[event]
+    }
+  }
+  once(event, callback) {
+    this.on(event, (...args) => {
+      callback(...args)
+      this.off(event)
+    })
+  }
+}
