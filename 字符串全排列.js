8c4fdad3ad9b013ec7578d0e0a00e57cf17e4f5b
TyroneYvesChen <243307272@qq.com>
2020-05-10 11:35:25 +0800

    'permutate'

---
+++ 字符串全排列.js
@@ -0,0 +1,18 @@
+function permutate(str) {
+  var array = str.split('')
+  function loop(array, pre = []) {
+    if (array.length == 1) {
+      return [pre.concat(array).join('')]
+    }
+    let res = []
+    for (let index = 0; index < array.length; index++) {
+      var first = array.pop()
+      res = res.concat(loop(array, [...pre, first]))
+      array.unshift(first)
+    }
+    return res
+  }
+  return Array.from(new Set(loop(array)))
+}
+
+console.log(permutate('123456'))
